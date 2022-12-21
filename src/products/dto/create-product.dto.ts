b50426ff4a7ddd23010b321e41ee6940e1f41fa2
SoHenriquez <estebansotohenriquez@gmail.com>
2022-12-21 16:04:40 -0300

    Product Dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,42 @@
-export class CreateProductDto {}
+import { 
+    IsString, 
+    IsInt, 
+    IsIn, 
+    IsNumber, 
+    IsOptional, 
+    IsPositive, 
+    MinLength, 
+    IsArray }
+from 'class-validator';
+
+
+export class CreateProductDto {
+    @IsString()
+    @MinLength(1)
+    title: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsString()
+    @IsOptional()
+    slug?: string;
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    stock?: number;
+    
+    @IsString({ each: true})
+    @IsArray()
+    sizes: string[];
+
+    @IsIn(['men','women','kid','unisex'])
+    gender: string;
+}
